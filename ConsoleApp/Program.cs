7f3bb6e1b73e86a1cb4732e54a7e87bf9a4fe3ea
Philipp Haider <haider.philipp@fronius.com>
2022-05-18 11:29:15 +0200

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Microsoft.FSharp.Collections;
+
+Console.WriteLine("Hello, World!");
+
+CultureInfo.CurrentCulture = new CultureInfo("de");
+CultureInfo.CurrentUICulture= new CultureInfo("de");
+
+try
+{
+    // Throws a localized ArgumentException
+    SeqModule.Head(SeqModule.Empty<int>());
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Handled: {ex.Message}");
+}
+
+Console.WriteLine("Wait a bit and press any key");
+
+Console.Read();
